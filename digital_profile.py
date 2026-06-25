b3b074c274038c62798b4dce3a0f55b696d2dfab
415-06-Peerawat <54257@yupparaj.ac.th>
2026-06-25 08:57:02 +0700

    digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,11 @@
+import streamlit as st
+st.tile("My Digital Profile")
+
+st.header("แนะนำตัว")
+st.write("นายพีระวัฒน์ ไชยวงค์")
+st.write("ชื่อเล่นปกป้อง")
+st.write("เลขที่ 6 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย:อยากสร้างเกม")
+st.write("สิ่งที่อยากเป็นในอนาคต:คนตัดต่อ")
